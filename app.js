e9e07799ca6b8a38f3e4e74a61fc7b8e01d839ff
Aman <amanfarhan2008@gmail.com>
2025-07-03 21:25:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// let age = 19;
+// age >= 18 ? console.log("adult ") : console.log("not adult");
+// console.log(result);
+
+// let age = 18;
+
+// if (age >= 18){
+//     console.log("you can vote")
+// } else{
+//     console.log("you can not vote")
+// }
+
+
+// let num = prompt("enter a number");
+
+// if (num % 5 === 0 ) {
+//     console.log(num, "is a multiple of 5");
+// } else {
+//     console.log(num, "is NOT a multiple of 5");
+// }
+
+let score = 89;
+let grade ;
+if (score >= 90 && score <=100){
+    grade = "A"
+} else if (score >= 70 && score <= 89){
+    grade = "B"
+} else if (score >= 50 && score <= 69){
+    grade = "C"
+}  else if (score >= 30 && score <= 49){
+    grade = "D"
+}  else if (score >= 10 && score <= 29){
+    grade = "FAIL"
+} console.log(grade)
