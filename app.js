d57f938fcd13b9ed31ea00dab2ff2674f3dadf29
Aman <amanfarhan2008@gmail.com>
2025-07-13 21:25:36 +0500

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let age = 19;
-age >= 18 ? console.log("adult ") : console.log("not adult");
-console.log(result);
+// let age = 19;
+// age >= 18 ? console.log("adult ") : console.log("not adult");
+// console.log(result);
 
 // let age = 18;
 
@@ -32,3 +32,140 @@ console.log(result);
 // }  else if (score >= 10 && score <= 29){
 //     grade = "FAIL"
 // } console.log(grade)
+
+
+
+// for loop
+// for(let i = 1 ;i<=5;  i++){
+//     console.log("apna collage")
+// }
+
+// for(let i = 1 ; i <= 5; i++ ){
+//     console.log("aman")
+// }
+
+// console.log(typeof)
+
+
+
+// let book = "A smarter way to learn JavaScript";
+// alert(book);
+
+
+// document.write("yeh! I can Write HTML Contect through javascript")
+
+
+// let store = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// alert(store);
+
+
+
+// let age = 17;
+// let age = prompt("enter your age")
+// alert( "your age is " + age)
+
+
+// let visitorName = "Doe";
+// let ProductTitle = "T-shirt(s)";
+// let Quantity = 5;
+
+// document.write(visitorName + " orderd " + Quantity + ProductTitle + " on XYZ clothing store");
+
+
+// let birthYear = 2008;
+// document.write("My birth year is : " +birthYear + "<br>");
+// document.write("date type of my decleared variable is number" );
+
+
+
+// let name , age , city ;
+
+// let myName, _age ,$value, user1, first_name;
+// let 1name , my-name , @home , #value;
+
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("<p>Variable ke naam mein sirf <b>letters</b>, <b>numbers</b>, <b>$</b> aur <b>_</b> hi use kar sakte hain.<br>Jaise: <b>$my_1stVariable</b></p>");
+
+
+
+
+// let num = 5;
+// let num1 = 8;
+// let sum = num + num1;
+// document.write("sum of " + num + " and "+ num1 + " is " + sum);
+
+
+// let num = 5;
+// let num1 = 3;
+// let subtraction = num - num1;
+// let multiplication = num * num1;
+// let division = num / num1;
+// let modulus = num % num1;
+
+// document.write("multiplication is " + multiplication + " :" + "<br>");
+// document.write("division is " + division + " :" + "<br>");
+// document.write("modulus is " + modulus + " :" + "<br>");
+// document.write("subtraction is " + subtraction+ " :" + "<br>");
+
+
+// let veriable ;
+// document.write("value after declear is ??" + veriable + "<br>");
+
+// veriable = 5;
+// document.write( "Initialize is " +veriable + "<br>");
+
+// veriable++;
+// document.write( "Value after increment is: " +veriable + "<br>");
+
+
+// veriable = veriable + 7;
+// document.write("value after additin is : " + veriable + "<br>");
+
+// veriable--;
+// document.write("value after decrimentation is : " + veriable + "<br>");
+
+// veriable = veriable % 3;
+// document.write("The reminder is : " + veriable);
+
+
+
+// let ticket = 600;
+// let total = ticket * 5;
+// document.write("Total cost to buy 5 tickets a movie is " + total + "PKR");
+
+
+// document.write("table of 5" + "<br>" + "<br>"+ "<br>");
+
+// let num = 5 ;
+// let num1 = 1;
+
+// document.write(num + " x " + num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>");
+// document.write(num + " x " + ++num1 + " = " + num*num1 + "<br>" + "<br>"+ "<br>" );
+// document.write(" end of table ");
+
+
+// let priceitem1 = 400;
+// let quantity1 = 3;
+// let priceitem2 = 300;
+// let quantity2 = 5;
+// let shippingcharges = 100
+// let totalitem1 = priceitem1 * quantity1;
+// let totalitem2 = priceitem2 * quantity2;
+// let totalCost = totalitem1 + totalitem2;
+
+// document.write("price of item1 : " + priceitem1 + "<br>");
+// document.write("quentity of item1: " + quantity1 + "<br>");
+// document.write("price of item2: " + priceitem2 + "<br>");
+// document.write("quzntity of item2 : " + quantity2 + "<br>");
+// document.write("shipping charges " + shippingcharges + "<br>" + "<br>");
+// document.write("total cost of your item is " + totalCost + "<br>");
+
